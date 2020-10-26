452b9770483237bb906689d0b5dd95fc6f552061
Nick Moore <nicholas.moore5@icloud.com>
2020-10-25 20:35:19 -0400

    add test to check for render of Home component for App

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,15 @@
-import React from "react";
-import { shallow, mount } from "enzyme";
-import App from "../App";
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import Home from '../components/Home'
+
 
 it("renders without crashing", () => {
   shallow(<App />);
+});
+
+it("renders Home component on page load", () => {
+  const wrapper = shallow(<App />);
+  const home = <Home />;
+  expect(wrapper.contains(home)).toEqual(true);
 });
